7783b3f3f4c7583217996709117b93d80b1ac9e9
Logan Hasson <logan.hasson@gmail.com>
2013-10-10 12:54:50 -0400

    Reset up

--- lib/student.rb
+++ lib/student.rb
@@ -1,13 +1,35 @@
 require 'sqlite3'
+require 'pry'
 
 class Student
   attr_accessor :name, :twitter, :linkedin, :facebook, :website
   attr_reader :id
-
+ 
   @@students = []
-
+ 
   @@db = SQLite3::Database.new('students.db')
 
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        twitter TEXT,
+        linkedin TEXT,
+        facebook TEXT,
+        website TEXT
+        );
+    SQL
+  end
+ 
+  def new_record?
+    !saved?
+  end
+
+  def saved?
+    @saved
+  end
+  
   def initialize(id = nil)
     @id = id
     if id.nil?
@@ -16,29 +38,29 @@ class Student
       else
         @id = @@students.max_by { |s| s.id }.id + 1
       end
-    end 
+    end
     @@students << self
     @saved = !id.nil?
   end
-
+ 
   def get_id
     cmd = "SELECT MAX(id) from STUDENTS"
     @id = @@db.execute(cmd).flatten[0]
   end
-
+ 
   def insert
     cmd = "INSERT INTO STUDENTS (name) VALUES (?)"
     @@db.execute(cmd, self.name)
     get_id
     @saved = true
   end
-
+ 
   def update
     cmd = "UPDATE STUDENTS set name = ? where id = ?"
     @@db.execute(cmd, self.name, self.id)
     @saved = true
   end
-
+ 
   def save
     if @saved
       update
@@ -46,36 +68,38 @@ class Student
       insert
     end
   end
-
+ 
   def self.reset_all
     @@students.clear
   end
-
+ 
   def self.all
     @@students
   end
-
+ 
   def self.find_by_name(name)
     @@students.select { |s| s.name == name }
   end
-
+ 
   def self.find(id)
     Student.load(id)
   end
-
+ 
   def self.delete(id)
     @@students.reject! { |s| s.id == id}
   end
-
+ 
   def self.load(id)
     cmd = "SELECT * FROM STUDENTS WHERE ID = ?"
     result = @@db.execute(cmd, id)
     Student.new_with_row(result.flatten)
   end
-
+ 
   def self.new_with_row(row)
     s = Student.new(row[0])
     s.name = row[1]
     s
   end
 end
+
+binding.pry
